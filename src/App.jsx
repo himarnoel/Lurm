ce9e20e343c1fe6e5025f2b9ae212e10e12b668d
himarnoel <olaimarnoel@gmail.com>
2022-12-03 00:35:14 +0100

    satrted working on ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Home from './Pages/Home'
+import Upload from './Pages/Upload';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a></a>
-        <a href="fms">sjdl</a>
-       
-        
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>z
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <a href="" className="p-5 bg-red-700 text-white">sdfaffa</a>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className=" bg-[#F2F2F2] w-screen h-screen">
+      <Home/>
+      
     </div>
   )
 }
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from '../assets/logo.png'
+const Home = () => {
+  return (
+    <div className='max-w-[1200px]   h-full mx-auto bg-red-500 flex justify-center items-center'>
+        {/* <img src={logo} alt="" className="sdf" /> */}
+    </div>
+  )
+}
+
+export default Home
