ad06ffe0648facbb3778a0370064ba7cc5e5cd57
himarnoel <olaimarnoel@gmail.com>
2022-12-03 17:26:05 +0100

    done now

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -24,12 +24,12 @@ const Home = () => {
         Resource Manager
       </p>
       {/* Search  */}
-      <div className="bg-white h-26 w-[90vw]  xl:w-[75vw]  lg:w-[82vw] h-10 flex justify-center items-center shadow-xl rounded-lg  mt-10 2xl:mt-28 md:mt-28">
+      <div className="bg-white h-26 w-[90vw]  xl:w-[75vw]  lg:w-[82vw] h-10 flex justify-center items-center shadow-xl rounded-lg  mt-10 xl:mt-24 2xl:mt-28 md:mt-28">
         <div class="absolute  left-2 bottom- flex items-center pl-3 ml-2 rounded-lg">
           <FiSearch
             size={22}
             color="green"
-            className="sm:ml-4 lg:ml-[4.5rem] xl:ml-42 2xl:ml-[10.9rem]"
+            className="sm:ml-4 lg:ml-[4.5rem] xl:ml-[9.8rem] 2xl:ml-[10.9rem]"
           />
         </div>
         <input
