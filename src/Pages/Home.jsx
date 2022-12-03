3c495e28915da345a5e8032f9bc3a6c3b932b3f7
himarnoel <olaimarnoel@gmail.com>
2022-12-03 13:38:27 +0100

    still working

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import logo from '../assets/logo.png'
 const Home = () => {
   return (
-    <div className='max-w-[1200px]   h-full mx-auto bg-red-500 flex justify-center items-center'>
-        {/* <img src={logo} alt="" className="sdf" /> */}
+    <div className='max-w-[1200px]   h-full mx-auto  flex justify-center items-center flex-col'>
+      <img src={logo} alt="" className="object-contain" />
+      <p className="text-2xl font-bold text-black">Landmark University</p>
+      <p className="text-xl font-bold text-black">Resource Manager</p>
+        
     </div>
   )
 }
