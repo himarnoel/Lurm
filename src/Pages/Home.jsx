5163389c3faceddf8a62885a92adb67895c1521f
himarnoel <olaimarnoel@gmail.com>
2022-12-17 21:12:27 +0100

    change the table ui

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -44,7 +44,7 @@ const Home = () => {
   };
 
   return (
-    <div className=" h-screen w-screen  lg:mx-auto  flex  items-center flex-col pt-16 lg:pt-10 sm:pt-32">
+    <div className=" h-screen w-screen  lg:mx-auto  flex  items-center flex-col pt-16 lg:pt-10  md:pt-32 sm:pt-32">
       <img src={logo} alt="" className="object-contain w-20 lg:" />
       <p className="text-xl lg:text-2xl font-bold text-black">
         Landmark University
@@ -54,59 +54,74 @@ const Home = () => {
       </p>
       {/* Search  */}
 
-      <div className="relative w-[87%]  sm:w-[89%] lg:w-[82%] xl:w-[75%] mt-14">
+      <div className="relative w-[89%]  sm:w-[89%] lg:w-[82%] xl:w-[75%] mt-14">
         <FiSearch
           size={22}
           color="green"
-          className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
+          className="absolute top-0 bottom-0 w-4 h-4 md:w-6 md:h-6 my-auto text-gray-400 left-3"
         />
         <input
           placeholder="search past questions by course code"
           onChange={(e) => Search(e.target.value)}
           type="text"
-          className="w-full py-2 pl-12 pr-4 text-black border rounded-lg outline-none bg-gray-50 focus:bg-white focus:border-green-600"
+          className="w-full md:py-2 py-[0.2rem] pl-12 pr-4 text-black border rounded-lg outline-none bg-gray-50 focus:bg-white focus:border-green-600"
         />
       </div>
       {/* List */}
+
       {bol ? (
         <img src={loade} alt="" className="w-10 object-contain mt-5" />
       ) : (
-        <div>
-          <div className=" w-[90vw] lg:w-[82vw] bg-white  xl:w-[75vw]  mt-5 flex flex-col rounded-lg   ">
-            {array.length !== 0 ? (
-              <div className="flex justify-evenly bg-white  xl:w-auto xl:pl-10 items-center text-[12px] mt-2 text-gray-500 lg:text-base w-[90vw] border-b-[0.1px] lg:w-[82vw]  border-gray-200  ">
-                <p>Course</p>
-                <p>Semester</p>
-                <p>Level</p>
-                <p>Session</p>
-                <p className="sm:hidden">download</p>
-                <span className="hidden sm:block">
-                  <p className="flex  ">Click to download</p>
-                </span>
-              </div>
-            ) : (
-              ""
-            )}
-            <div className=" max-h-[40vh]  overflow-y-scroll rounded-lg">
-              {array.map((arr, i) => (
-                <div
-                  key={i}
-                  className="flex justify-evenly items-center text-[12px] lg:text-[15px] lg:w-[72vw] ml-2 w-[86vw] sm:w-[85vw]  bg-white pt-3 rounded-lg text-black  font-semibold pb-5 "
-                >
-                  <p>{arr.courseCode}</p>
-                  <p>{arr.semester}</p>
-                  <p>{arr.level}</p>
-                  <p>{arr.session}</p>
-
-                  <AiOutlineDownload
-                    onClick={() => Download(arr)}
-                    size="16"
-                    className="sm:text-5xl "
-                  />
-                </div>
-              ))}
+        <div className="md:mt-5 mt-3">
+          {array.length !== 0 ? (
+            <div className="overflow-y-auto lg:w-fit max-h-[40vh] w-[90vw] rounded-md ">
+              <table className="table-auto lg:w-[890px] xl:w-[1020px] w-[90vw]  bg-white text-center ">
+                <thead className="border-b sticky top-0  bg-white font-normal text-xs md:text-base">
+                  <tr>
+                    <th scope="col" className="sticky top-0  px-1">
+                      Course
+                    </th>
+                    <th scope="col" className="sticky top-0  px-1">
+                      Semester
+                    </th>
+                    <th scope="col" className="sticky top-0 px-1">
+                      Level
+                    </th>
+                    <th scope="col" className="sticky top-0 px-1">
+                      Session
+                    </th>
+                    <th scope="col" className="sticky top-0 px-1">
+                      Download
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="   ">
+                  {array.map((arr, i) => (
+                    <tr className="hover:bg-gray-50 dark:hover:bg-gray-600 text-xs md:text-base">
+                      <td className="py-2 px-3 md:px-4 lg:px-6">
+                        {arr.courseCode}
+                      </td>
+                      <td className="py-2 px-3 md:px-4 lg:px-6">
+                        {arr.semester}
+                      </td>
+                      <td className="py-2 px-3 md:px-4 lg:px-6">{arr.level}</td>
+                      <td className="py-2 px-3 md:px-4 lg:px-6">
+                        {arr.session}
+                      </td>
+                      <td className="py-2 px-3 md:px-4 lg:px-6">
+                        <AiOutlineDownload
+                          onClick={() => Download(arr)}
+                          className="mx-auto"
+                        />
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </div>
-          </div>
+          ) : (
+            ""
+          )}
         </div>
       )}
     </div>
