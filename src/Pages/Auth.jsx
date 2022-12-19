d67f5949ca906fbbf9a816f32682c352105df5e0
himarnoel <olaimarnoel@gmail.com>
2022-12-20 00:45:10 +0100

    done

--- src/Pages/Auth.jsx
+++ src/Pages/Auth.jsx
@@ -5,7 +5,7 @@ import { HiOutlineKey } from "react-icons/hi";
 const Auth = () => {
   return (
     <div className="flex flex-col items-center  h-screen w-screen ">
-      <div className="flex mt-14 sm:mt-14 md:mt-10">
+      <div className="flex mt-14 sm:mt-10 md:mt-12">
         <img src={logo} alt="" className="object-contain w-14 mr-5" />
         <div>
           <p className="text-lg lg:text-xl font-bold text-black">
@@ -16,8 +16,9 @@ const Auth = () => {
           </p>
         </div>
       </div>
-      <div className="  sm:mt-14  md:mt-10  mt-14 w-[21rem] h-[20rem] md:h-[24rem] lg:w-[30rem] lg:h-[24rem] sm:w-[30rem] xl:h-[26rem] xl:w-[30rem]   bg-white shadow-lg rounded-lg flex flex-col items-center ">
-        <div className="relative w-72 sm:w-[25rem] mt-10 ">
+      <div className="  sm:mt-10  md:mt-12  mt-14 w-[21rem] h-[20rem]  md:w-[23rem] md:h-[20rem] lg:w-[22rem] lg:h-[20rem] sm:w-[24rem] xl:h-[20rem] xl:w-[25rem]   bg-white shadow-lg rounded-lg flex flex-col items-center ">
+       <div className="xl:w-[22rem]">
+       <div className="relative  w-full mt-10 xl:mt-10">
           <label htmlFor="">username</label>
           <AiOutlineUser
             size={20}
@@ -30,7 +31,7 @@ const Auth = () => {
             type="text"
           />
         </div>
-        <div className="relative w-72 mt-5 xl:mt-6 sm:w-[25rem]">
+        <div className="relative w-full mt-5">
           <label htmlFor="">Password</label>
           <HiOutlineKey
             size={20}
@@ -43,9 +44,10 @@ const Auth = () => {
             type="text"
           />
         </div>
-        <button className="w-72 py-1 xl:py-2 sm:w-[25rem] bg-[#2F8B33] rounded-lg mt-10 xl:mt-12 text-white text-sm">
+        <button className=" py-1 xl:py-2 w-full bg-[#2F8B33] rounded-lg mt-10 xl:mt-12 text-white text-sm">
           Log in
         </button>
+       </div>
       </div>
     </div>
   );
