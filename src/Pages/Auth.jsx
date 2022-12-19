b5225585167a0d9c63ddea0bef0dba70265784e1
himarnoel <olaimarnoel@gmail.com>
2022-12-20 00:51:35 +0100

    done with ui

--- src/Pages/Auth.jsx
+++ src/Pages/Auth.jsx
@@ -17,37 +17,37 @@ const Auth = () => {
         </div>
       </div>
       <div className="  sm:mt-10   md:mt-12  mt-14 w-[20rem] h-[20rem]  md:w-[23rem] md:h-[20rem] lg:w-[22rem] lg:h-[20rem] sm:w-[24rem] xl:h-[20rem] xl:w-[25rem]   bg-white shadow-lg rounded-lg flex flex-col items-center ">
-       <div className="xl:w-[22rem] w-[17rem]">
-       <div className="relative  w-full mt-10 xl:mt-10">
-          <label htmlFor="">username</label>
-          <AiOutlineUser
-            size={20}
-            color="green"
-            className="absolute top-8 right-2 "
-          />
-          <input
-            placeholder="aremu.charity"
-            className="border p-2 rounded-md w-full  focus:outline-[#D1D1D1]"
-            type="text"
-          />
+        <div className="xl:w-[22rem] w-[17rem]">
+          <div className="relative  w-full mt-10 xl:mt-10">
+            <label htmlFor="">username</label>
+            <AiOutlineUser
+              size={20}
+              color="green"
+              className="absolute top-8 right-2 "
+            />
+            <input
+              placeholder="aremu.charity"
+              className="border p-2 rounded-md w-full  focus:outline-[#D1D1D1]"
+              type="text"
+            />
+          </div>
+          <div className="relative w-full mt-5">
+            <label htmlFor="">Password</label>
+            <HiOutlineKey
+              size={20}
+              color="green"
+              className="absolute top-8 right-2"
+            />
+            <input
+              placeholder="aremu.charity"
+              className="border p-2 rounded-md w-full focus:outline-[#D1D1D1] "
+              type="text"
+            />
+          </div>
+          <button className=" py-1 xl:py-2 w-full bg-[#2F8B33] rounded-lg mt-10 xl:mt-12 text-white text-sm">
+            Log in
+          </button>
         </div>
-        <div className="relative w-full mt-5">
-          <label htmlFor="">Password</label>
-          <HiOutlineKey
-            size={20}
-            color="green"
-            className="absolute top-8 right-2"
-          />
-          <input
-            placeholder="aremu.charity"
-            className="border p-2 rounded-md w-full focus:outline-[#D1D1D1] "
-            type="text"
-          />
-        </div>
-        <button className=" py-1 xl:py-2 w-full bg-[#2F8B33] rounded-lg mt-10 xl:mt-12 text-white text-sm">
-          Log in
-        </button>
-       </div>
       </div>
     </div>
   );
