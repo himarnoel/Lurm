f56ebced276228fe57c10e2cca025df3dce9ff64
himarnoel <olaimarnoel@gmail.com>
2022-12-22 02:17:54 +0100

    let see

--- src/Pages/Auth.jsx
+++ src/Pages/Auth.jsx
@@ -133,7 +133,18 @@ const Login = () => {
               </form>
             </div>
           </div>
-          <Footer />
+          <div className=" py-5 pl-5 lg:px-10 md:text-sm w-screen text-[0.6rem]  bg-white flex justify-between items-center">
+        <div className="w-36">©Copyright Holders </div>
+        <div className=" md:w-[27rem]  w-64 flex justify-evenly ">
+          <Link to="/login">Upload Documents</Link>
+          <a href="https://cms.lmu.edu.ng" target="_blank">
+            CMS
+          </a>
+          <a href="https://lmu.edu.ng" target="_blank">
+            School Website
+          </a>
+        </div>{" "}
+      </div>
         </div>
       </div>
     </div>
