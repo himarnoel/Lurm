d230ed374f601c39489216be833cc270f68b67c6
himarnoel <olaimarnoel@gmail.com>
2022-12-24 19:44:24 +0100

    added a Link

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
 import logo from "../assets/logo.png";
-
+import { Link } from "react-router-dom";
 const Nav = () => {
   return (
     <div> <div className="lg:w-screen py-2 px-5 md:px-10 bg-white shadow-sm md:shadow-md lg:shadow-lg   w-screen">
-    <div className="flex font-bold  items-center ">
+ <Link to="/">   <div className="flex font-bold  items-center ">
       <img src={logo} alt="" className="object-contain w-10 lg:" />{" "}
       <span className="flex flex-col leading-[1.2rem] pl-2">
         <span>Landmark University</span>
         <span>Resources Manager</span>
       </span>
     </div>
+    </Link>
   </div></div>
   )
 }
